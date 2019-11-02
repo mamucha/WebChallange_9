3c6bb0da529165021164cb4eab070852d0f42172
Marcin <marcin9213@gmail.com>
2019-11-02 18:41:26 +0100

    add slider section opinion

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,2 @@
 require('../scss/components/c-menu/c-menu.js');
+require('../scss/components/c-opinion/c-opinion.js');
---
+++ src/scss/components/c-opinion/c-opinion.js
@@ -0,0 +1,7 @@
+import Glide from '@glidejs/glide'
+
+new Glide('.glide').mount({
+    type: 'carousel',
+    startAt: 0,
+    perView: 1
+})
